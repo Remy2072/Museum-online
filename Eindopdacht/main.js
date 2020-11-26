704b598136bf714200f47b391e0e368f1e571a3a
Remy2072 <30528@ma-web.nl>
2020-11-26 21:49:07 +0100

    update website

--- Eindopdacht/main.js
+++ Eindopdacht/main.js
@@ -7,3 +7,18 @@ function cursorMovement(e) {
     cursor.style.top = cursorY + "px";
     console.log(cursorX + "px");
 }
+var navMenu = document.querySelector('nav');
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click',() =>{
+    if(!menuOpen){
+        navMenu.classList.add('zichtbaar');
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        navMenu.classList.remove('zichtbaar');
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
